9d82588249ba2d9c01ad313ca1c42b899bbd3586
Kaua-Moura-Pinho <kauamourapinho@gmail.com>
2024-05-13 21:15:08 -0300

    First commit

---
+++ src/routes.ts
@@ -0,0 +1,36 @@
+import { Router } from "express";
+
+import multer from "multer";
+import uploadConfig from "./config/multer";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
+import { CreateProductController } from "./controllers/product/CreateProductController";
+import { AuthUserController } from "./controllers/user/AuthUserController";
+import { CreateUserController } from "./controllers/user/CreateUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+import { isAuthenticated } from "./services/user/middlewares/IsAuthenticated";
+import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
+import { CreateOrderController } from "./controllers/order/CreateOrderController";
+import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
+import { AddItemController } from "./controllers/order/AddItemController";
+import { SendOrderController } from "./controllers/order/SendOrderController"; 
+
+
+
+const router: Router = Router();
+const upload = multer(uploadConfig.upload('./tmp'));
+
+router.post('/user', new CreateUserController().handle);
+router.post('/session', new AuthUserController().handle);
+router.get('/userinfo', isAuthenticated, new DetailUserController().handle);
+router.post('/category', isAuthenticated, new CreateCategoryController().handle);
+router.get('/categories', isAuthenticated, new ListCategoryController().handle);
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle);
+router.get('/category/product', isAuthenticated, new ListByCategoryController().handle);
+router.post('/order', isAuthenticated, new CreateOrderController().handle);
+router.delete('/deleteorder', isAuthenticated, new RemoveOrderController().handle);
+router.post('/order/add', isAuthenticated, new AddItemController().handle);
+router.put('/order/send', isAuthenticated, new SendOrderController().handle);
+
+export { router };
+
