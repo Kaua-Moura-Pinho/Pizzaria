0ba10364b3f99529701832092d7b36c577117bb7
unknown <kauamourapinho@gmail.com>
2024-06-03 21:28:47 -0300

    commit

--- prisma/migrations/20240604002318_migrate/migration.sql
+++ prisma/migrations/20240604002318_migrate/migration.sql
@@ -36,7 +36,7 @@ CREATE TABLE "pedido" (
 );
 
 -- CreateTable
-CREATE TABLE "Item" (
+CREATE TABLE "item" (
     "id" TEXT NOT NULL,
     "quantidade" INTEGER NOT NULL,
     "criado_em" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
@@ -44,14 +44,14 @@ CREATE TABLE "Item" (
     "id_pedido" TEXT NOT NULL,
     "id_produto" TEXT NOT NULL,
 
-    CONSTRAINT "Item_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "item_pkey" PRIMARY KEY ("id")
 );
 
 -- AddForeignKey
 ALTER TABLE "produto" ADD CONSTRAINT "produto_id_categoria_fkey" FOREIGN KEY ("id_categoria") REFERENCES "categoria"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Item" ADD CONSTRAINT "Item_id_pedido_fkey" FOREIGN KEY ("id_pedido") REFERENCES "pedido"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "item" ADD CONSTRAINT "item_id_pedido_fkey" FOREIGN KEY ("id_pedido") REFERENCES "pedido"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Item" ADD CONSTRAINT "Item_id_produto_fkey" FOREIGN KEY ("id_produto") REFERENCES "produto"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "item" ADD CONSTRAINT "item_id_produto_fkey" FOREIGN KEY ("id_produto") REFERENCES "produto"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
